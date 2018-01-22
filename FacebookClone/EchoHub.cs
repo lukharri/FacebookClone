5b663edc073d8baf15455440575e5fb1487ef2ba
Luke Harris <luke.e.harris@gmail.com>
2018-01-22 14:37:51 -0600

    Project setup

---
+++ FacebookClone/EchoHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace FacebookClone
+{
+    [HubName("echo")]
+    public class EchoHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
---
+++ FacebookClone/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FacebookClone.Startup))]
+
+namespace FacebookClone
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
